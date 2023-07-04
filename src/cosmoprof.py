f8b8cdd7b81666403e1e9beca2e04e013d8ceae9
bwj2800 <bwj2800@naver.com>
2023-07-04 16:46:13 +0900

    FEAT saving result in csv format

--- src/cosmoprof.py
+++ src/cosmoprof.py
@@ -15,12 +15,13 @@ from selenium.webdriver import ActionChains
 from tqdm import tqdm
 import time
 import logging
-
+import pandas as pd
 
 warnings.filterwarnings('ignore')
 options = Options()
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
 options.add_argument('user-agent=' + user_agent)
+options.add_argument('--ignore-certificate-errors')
 # ## for background
 # options.add_argument("headless")
 # options.add_argument('--window-size=1920, 1080')
@@ -39,9 +40,9 @@ options.add_argument('user-agent=' + user_agent)
 
 class CosmoprofCrawler:
 
-    def __init__(self, url, save_path):
+    def __init__(self, url, save_dir):
         self.url = url        
-        self.save_path = save_path
+        self.save_dir = save_dir
         
     def __wait_until_find_xpath(self, driver, xpath):
         WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, xpath)))
@@ -58,33 +59,38 @@ class CosmoprofCrawler:
         button = driver.find_element(By.XPATH, xpath)
         driver.execute_script("arguments[0].click();", button)
         
-    # def save_data(self, id, status, username, code, extension):
-    #     # Save Code
-    #     status = "".join([word.upper() for word in status if word.strip()])
-    #     if status in ["AC"]:
-    #         result = "correct"
-    #     elif status in ["WA", "PAC"]:
-    #         result = "wrong"
-    #     else:
-    #         result = "error"
-    #     dir_path = os.path.join(self.save_path)
-    #     file_path = dir_path+'/'+str(id)+'&'+status+'&'+str(username)+extension
-    #     self.save(dir_path, file_path, code)    
+    def save_data(self, name, country, website, sns, source):
+        df = pd.DataFrame(name, columns = ['name'])
+        df['country'] = country
+        df['website'] = website
+        df['sns'] = sns
+        df['source'] = source
+        
+        # df.to_csv("cosmoprofData/cosmoprof.csv", index = False)
+        
+        self.save("cosmoprof.csv", df)
+           
     
-    # def save(self, dir_path, file_path, data):
-    #     if not os.path.isdir(dir_path):
-    #         os.makedirs(dir_path)
-    #     with open(file_path, 'w') as w:
-    #         w.write(data.strip())
+    def save(self, file_name, dataframe):
+        if not os.path.isdir(self.save_dir):
+            os.makedirs(self.save_dir)
+            
+        dataframe.to_csv(self.save_dir+file_name, index = False)
             
     def get_data(self, driver):
         driver.switch_to.window(driver.window_handles[-1])
-        self.__wait_until_find_classname(driver,"BoothContactCountry")
+        self.__wait_until_find_classname(driver,"BoothContactInfo.pull-left")
         
-        country = driver.find_element(By.CLASS_NAME, 'BoothContactCountry').text
-        website = driver.find_element(By.CLASS_NAME, "BoothContactUrl").text
+        try:
+            country = driver.find_element(By.CLASS_NAME, 'BoothContactCountry').text
+        except:
+            country = ''
         
-        # sns=[]
+        try:
+            website = driver.find_element(By.CLASS_NAME, "BoothContactUrl").text
+        except:
+            website = ''
+        
         # sns=[facebook,twitter,linkedin,instagram]
         sns=['','','','']
         try:
@@ -116,6 +122,12 @@ class CosmoprofCrawler:
         # 쿠키 허용
         driver.find_element(By.CLASS_NAME, "cc-compliance").click()
         
+        name_all=[]
+        country_all=[]
+        website_all=[]
+        sns_all=[]
+        source_all=[]
+        
         companies=driver.find_elements(By.CLASS_NAME, "exhibitorName")
         # ActionChains(driver).move_to_element(names[0]).perform()
         
@@ -136,19 +148,28 @@ class CosmoprofCrawler:
             # print(country,website, sns)
             
             name=company.text.strip()
+            source="COSMOPROF"
             
             # 기업명 클릭
             company.click()
             
             country, website, sns = self.get_data(driver)
+            if website=='' and sns[0]=='': # 웹사이트, 페이스북 링크 모두 없는 경우에 데이터 제외
+                print("empty data", name)
+            elif country=='Korea (South)': # 한국 바이어 데이터 제외
+                print("Korea data", name)
+            else:
+                name_all.append(name)
+                country_all.append(country)
+                website_all.append(website)
+                sns_all.append(sns)
+                source_all.append(source)
+                print(name)
+                # print('|', name,'|', country,'|', website,'|', sns,'|', source)
             
-            print('|', name,'|', country,'|', website,'|', sns)
-            
-    
-
         driver.quit()
-
-        return name, country, website, sns
+        
+        return name_all, country_all, website_all, sns_all, source_all
     
     # def set_txt(self, id, table_name, cols):
     #     f_name = str(id)+".txt"
@@ -165,12 +186,12 @@ class CosmoprofCrawler:
         
 if __name__ == '__main__':
     url="https://s23.a2zinc.net/clients/pba/cosmoprof2022/Public/Exhibitors.aspx?Index=All"
-    save_path = 'cosmoprofData/'
+    save_dir = 'cosmoprofData/'
     
     # Run Crawler with save_path
-    cc = CosmoprofCrawler(url, save_path)
+    cc = CosmoprofCrawler(url, save_dir)
     
-    # Run Only One Project  
-    name, country, website, sns = cc.run()
-    # ccc.save_data(name, sns)
+    name_all, country_all, website_all, sns_all, source_all = cc.run()
+    
+    cc.save_data(name_all, country_all, website_all, sns_all, source_all)
             
