50cf4e5e5bc1c2cc12307b096cbbd7d78110fbc3
bwj2800 <bwj2800@naver.com>
2023-07-06 16:02:33 +0900

    FIX icon src matching condition & REFACTOR driver

--- src/facebook.py
+++ src/facebook.py
@@ -6,7 +6,7 @@ import warnings
 from tqdm import tqdm
 import logging
 from dotenv import load_dotenv
-from lxml import etree
+# from lxml import etree
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -24,13 +24,13 @@ options = Options()
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 options.add_argument('user-agent=' + user_agent)
 ## for background
-options.add_argument("headless")
-options.add_argument('--window-size=1920, 1080')
-options.add_argument('--no-sandbox')
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument('--start-maximized')
-options.add_argument('--start-fullscreen')
-options.add_argument('--disable-blink-features=AutomationControlled')
+# options.add_argument("headless")
+# options.add_argument('--window-size=1920, 1080')
+# options.add_argument('--no-sandbox')
+# options.add_argument("--disable-dev-shm-usage")
+# options.add_argument('--start-maximized')
+# options.add_argument('--start-fullscreen')
+# options.add_argument('--disable-blink-features=AutomationControlled')
 
 # Save log 
 # logger = logging.getLogger()
@@ -41,42 +41,46 @@ options.add_argument('--disable-blink-features=AutomationControlled')
 ## For Crawling the Facebook page
 class FacebookCrawler:
     
-    def __init__(self, save_path):
+    def __init__(self, save_path, driver):
         self.save_path = save_path
-        
+        self.driver = driver
         load_dotenv()
 
         ## For facebook login
         self.FACEBOOK_ID = os.environ.get('FACEBOOK_ID')
         self.FACEBOOK_PW = os.environ.get('FACEBOOK_PW')
         
-    def __wait_until_find(self, driver, xpath):
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, xpath)))
-        element = driver.find_element(By.XPATH, xpath)
+    def __wait_until_find(self, xpath):
+        WebDriverWait(self.driver, 7).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        element = self.driver.find_element(By.XPATH, xpath)
         return element
             
-    def __wait_and_click(self, driver, xpath):
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-        button = driver.find_element(By.XPATH, xpath)
-        driver.execute_script("arguments[0].click();", button)
+    def __wait_and_click(self, xpath):
+        WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        button = self.driver.find_element(By.XPATH, xpath)
+        self.driver.execute_script("arguments[0].click();", button)
 
     def get_icon(self, src):
         ## For make category using src of icon
         icon = ''
         if src in ['https://static.xx.fbcdn.net/rsrc.php/v3/yC/r/qF_eflLVarp.png', \
-            'https://static.xx.fbcdn.net/rsrc.php/v3/ye/r/4PEEs7qlhJk.png']:
+            'https://static.xx.fbcdn.net/rsrc.php/v3/ye/r/4PEEs7qlhJk.png', \
+            'https://static.xx.fbcdn.net/rsrc.php/v3/yj/r/XLGk7XTX1NS.png']:
             icon = 'info'
         elif src in ['https://static.xx.fbcdn.net/rsrc.php/v3/y0/r/Oc_idC-xUXw.png', \
             'https://static.xx.fbcdn.net/rsrc.php/v3/yM/r/qUPkXkiEwfh.png']:
             icon = 'owner'
         elif src in ['https://static.xx.fbcdn.net/rsrc.php/v3/yr/r/bwmGKGh4YjO.png', \
-            'https://static.xx.fbcdn.net/rsrc.php/v3/yW/r/8k_Y-oVxbuU.png']:
+            'https://static.xx.fbcdn.net/rsrc.php/v3/yW/r/8k_Y-oVxbuU.png', \
+            'https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/V632KGZoHho.png']:
             icon = 'address'
         elif src in ['https://static.xx.fbcdn.net/rsrc.php/v3/yb/r/KVUi1wUrbfb.png', \
-            'https://static.xx.fbcdn.net/rsrc.php/v3/yE/r/2PIcyqpptfD.png']:
+            'https://static.xx.fbcdn.net/rsrc.php/v3/yE/r/2PIcyqpptfD.png', \
+            'https://static.xx.fbcdn.net/rsrc.php/v3/yO/r/W4m-1QXtJyK.png']:
             icon = 'email'
         elif src in ['https://static.xx.fbcdn.net/rsrc.php/v3/y4/r/UF-jk_lKW5x.png', \
-            'https://static.xx.fbcdn.net/rsrc.php/v3/y3/r/BQdeC67wT9z.png']:
+            'https://static.xx.fbcdn.net/rsrc.php/v3/y3/r/BQdeC67wT9z.png', \
+            'https://static.xx.fbcdn.net/rsrc.php/v3/y-/r/DzX7o-tOmJ6.png']:
             icon = 'site_url'
         else:
             icon = 'etc'
@@ -97,21 +101,19 @@ class FacebookCrawler:
             desc = text
         return desc
     
-    def get_icon_list(self, driver):
+    def get_icon_list(self):
         ## Crawling the icons
         icon_list = {}
-        icon_list_xpath = '/html/body/div[1]/div/div[1]/div/div[3]/\
-            div/div/div/div[1]/div[1]/div/div/div[4]/div[2]/\
-            div/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/div[2]/div/ul'
-            
+        icon_list_xpath = '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/div[2]/div/ul'
+                        
         try:
-            icon_el_list = self.__wait_until_find(driver, icon_list_xpath)
+            icon_el_list = self.__wait_until_find(icon_list_xpath)
         except:
             try:
                 icon_list_xpath = '/html/body/div[1]/div/div[1]/div/div[3]/\
                     div/div/div/div[1]/div[1]/div/div/div[4]/div[2]/\
                     div/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/div[1]/div/ul'
-                icon_el_list = self.__wait_until_find(driver, icon_list_xpath)
+                icon_el_list = self.__wait_until_find(icon_list_xpath)
             except:
                 print("No List")
                 return {}
@@ -132,24 +134,24 @@ class FacebookCrawler:
             
         return icon_list
     
-    def click_escape_key(self, driver):
+    def click_escape_key(self):
         ## When login popup shows
         try:
-            action = ActionChains(driver)
+            action = ActionChains(self.driver)
             action.send_keys(Keys.ESCAPE).perform()
             time.sleep(1)
         except:
             print("Click Error")
             
-    def login(self, driver):
-        ## When the page goes to login page
+    def login(self):
+        # When the page goes to login page
         try:
-            id_field = self.__wait_until_find(driver, '//*[@id="email"]')
+            id_field = self.__wait_until_find('//*[@id="email"]')
             id_field.send_keys(self.FACEBOOK_ID)
-            pw_field = self.__wait_until_find(driver, '//*[@id="pass"]')
+            pw_field = self.__wait_until_find('//*[@id="pass"]')
             pw_field.send_keys(self.FACEBOOK_PW)
 
-            self.__wait_and_click(driver, '//*[@id="loginbutton"]')
+            self.__wait_and_click('//*[@id="loginbutton"]')
             
         except:
             print('Fail Login')
@@ -166,16 +168,13 @@ class FacebookCrawler:
     def run_one(self, url):
         
         ## Go to facebook page
-        driver = self.get_driver(url)
+        self.driver.get(url)
         
-        if 'https://www.facebook.com/login' in driver.current_url:
-            return {}
-        
-        ## Close the login popup
-        self.click_escape_key(driver)
+        # if 'https://www.facebook.com/login' in driver.current_url:
+        #     return {}
         
         ## Get list of facebook page description
-        icon_list = self.get_icon_list(driver)
+        icon_list = self.get_icon_list()
         # print(icon_list)
         return icon_list
         
